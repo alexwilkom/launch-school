c85b589f2fab7f3f18597d293d65f2dfb8f5b641
Alex Wilkom <alex.wilkom@icloud.com>
2024-11-19 10:50:51 +0000

    Do exercise 7, chapter 9

---
+++ ls-intro-ruby-book/09-exercises/ex-7.rb
@@ -0,0 +1,4 @@
+# What's the major difference between an Array and a Hash?
+
+# The main difference is that to refer a specific data the Array uses numbered key indexes, whereas
+# Hash uses key-value pairs.
