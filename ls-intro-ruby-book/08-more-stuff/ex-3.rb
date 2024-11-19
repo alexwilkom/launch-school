783f86760c627b3496271de953370910bae38b81
Alex Wilkom <alex.wilkom@icloud.com>
2024-11-19 10:23:40 +0000

    Do exercise 3, chapter 8

---
+++ ls-intro-ruby-book/08-more-stuff/ex-3.rb
@@ -0,0 +1,3 @@
+# What is exception handling and what problem does it solve?
+# Exception handling is a way to handle errors that may occur in the code.
+# It prevents the program to exiting unexpectedly.
