1690f05236110c49add66c3ad2ea7193868b3dc4
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-01 12:19:56 +0000

    Change structure and names of directories

--- exercises/ruby-basics/methods/ex_1.rb
+++
@@ -1,7 +0,0 @@
-# Write a method named print_me that prints I'm printing within the method!
-
-def print_me
-  puts "I'm printing within the method!"
-end
-
-print_me
--- exercises/ruby-basics/methods/ex_2.rb
+++
@@ -1,8 +0,0 @@
-# Write a method named print_me so that I'm printing the return value! is printed when running the following code:
-# puts print_me 
-
-def print_me
-  "I'm printing the return value!"
-end
-
-puts print_me
