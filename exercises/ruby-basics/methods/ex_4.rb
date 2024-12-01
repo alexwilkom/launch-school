e8545a18659e6c6db59c6494ba0e5ce41ee8948b
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-01 11:54:45 +0000

    Do exercise 5, methods

---
+++ exercises/ruby-basics/methods/ex_4.rb
@@ -0,0 +1,22 @@
+# Write a method named greet that invokes the following methods:
+#   def hello
+#     'Hello'
+#   end
+
+#   def world
+#     'World'
+#   end
+
+def hello
+  'Hello'
+end
+
+def world
+  'World'
+end
+
+def greet
+  "#{hello} #{world}"
+end
+
+puts greet
