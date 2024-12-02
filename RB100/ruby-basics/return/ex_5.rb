ff16a3a8cbb12f7fcd80f5367d7a44a62e6687c8
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-02 22:06:53 +0000

    Do exercise 5, return

---
+++ RB100/ruby-basics/return/ex_5.rb
@@ -0,0 +1,12 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+def meal
+  'Dinner'
+  puts 'Dinner'
+end
+
+p meal
+
+# "Dinner"
+# nil
+# As in ex_4.rb the last line of the meal method is evaluated and the value returned.
+# In this case is nil, as puts always returns nil.
