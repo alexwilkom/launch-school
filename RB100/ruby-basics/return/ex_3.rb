7b33bad5b83109f462f579a9d6bbeae5cbc92ea3
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-02 21:58:24 +0000

    Do exercise 3, return

---
+++ RB100/ruby-basics/return/ex_3.rb
@@ -0,0 +1,11 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+def meal
+  return 'Breakfast'
+  'Dinner'
+end
+
+puts meal
+
+# "Breakfast"
+# When return keyword is encountered, it returns the evaluated result of the line
+# and exits the method execution.
