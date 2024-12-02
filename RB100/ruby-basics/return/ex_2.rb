a16a5f06910f175d94b9a86d06f4e8c3063a8393
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-02 21:56:14 +0000

    Do exercise 2, return

---
+++ RB100/ruby-basics/return/ex_2.rb
@@ -0,0 +1,9 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+def meal
+  'Evening'
+end
+
+puts meal
+
+# "Evening"
+# The last line of the method returns the evaluated value. No need for the return keyword.
