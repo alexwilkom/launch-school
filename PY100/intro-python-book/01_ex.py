5d1aba0b799419c1f494517bbdf4da13c3304fcd
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-21 14:18:58 +0000

    Do exercise 3, basic operations

--- PY100/intro-python-book/01_ex.py
+++
@@ -1,2 +0,0 @@
-# Concatenate two strings, one with your first name and one with your last, to create a new string with your full name as its value.
-print('Alex' + ' ' + 'Wilkom')
---
+++ PY100/intro-python-book/02-basic-operations/03_ex.py
@@ -0,0 +1,2 @@
+# What does the following code do? Why?
+print('5' + '10') # This is a string cocatenation. Text is combined: '510'.
