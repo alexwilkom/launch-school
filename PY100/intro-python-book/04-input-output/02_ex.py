f2a4e517d8fe4d73414b0d7e7a1a16d93aefc99c
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-22 13:55:46 +0000

    Do exercise 2, input output

---
+++ PY100/intro-python-book/04-input-output/02_ex.py
@@ -0,0 +1,5 @@
+# Modify the previous program to ask for the user's first and last names separately, then greet the user with their full name.
+
+name = input('What is your name? ')
+last_name = input('What is your last name? ')
+print(f'Hello {name} {last_name}!')
