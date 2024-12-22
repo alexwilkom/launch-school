47a0e257a708609d1798b4fd0571163df1665444
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-22 12:56:50 +0000

    Do exercise 4, variables

---
+++ PY100/intro-python-book/03-variables/04_ex.py
@@ -0,0 +1,9 @@
+# Classify the following potential class names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# index             non-idiomatic. Must start with a capital letter
+# CatName           idiomatic.
+# Lazy_Dog          non-idiomatic. Must use Camel Case. No underscore.
+# 1ST               illegal. Can't start with a digit.
+# operand2          non-idiomatic. Must start with a capital letter
+# BigNumber3        idiomatic
+# Πi                non-idiomatic. Π is not part of ASCII
