374435ab4388ffdc803e5f689f691b2333bf024c
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-22 12:01:44 +0000

    Do exercise 1, variables

---
+++ PY100/intro-python-book/03-variables/01_ex.py
@@ -0,0 +1,10 @@
+# Classify the following potential non-constant variable names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# index             idiomatic
+# CatName           non-idiomatic. They should not use capital letters.
+# lazy_dog          idiomatic
+# quick_Fox         non-idiomatic. Fox should be fox. No capital letters.
+# 1stCharacter      illegal. It should not start with a digit.
+# operand2          idiomatic
+# BIG_NUMBER        non-idiomatic. They should not use capital letters.
+# π                 non-idiomatic. π is a special character not in ASCII.
