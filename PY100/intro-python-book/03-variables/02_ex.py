19d2d3e5024adfe4c576d6f50f5713651851b019
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-22 12:03:44 +0000

    Do exercise 2, variables

---
+++ PY100/intro-python-book/03-variables/02_ex.py
@@ -0,0 +1,3 @@
+# Classify the following potential function names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Same as previous question!
