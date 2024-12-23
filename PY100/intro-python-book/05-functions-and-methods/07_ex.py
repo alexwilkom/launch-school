72a3cac6e69983473688ecc70f06de319c4b9a03
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-23 08:14:03 +0000

    Do exercise 7, functions and methods

---
+++ PY100/intro-python-book/05-functions-and-methods/07_ex.py
@@ -0,0 +1,7 @@
+# Without running the following code, what do you think it will do?
+
+def foo(bar, qux):
+    print(bar)
+    print(qux)
+
+foo('Hello') # Exception raised. foo needs two arguments, but only one is passed.
