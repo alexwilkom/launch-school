01a4b13e00085bb3893a945c54f6bf3de878a593
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-23 08:20:32 +0000

    Do exercise 11, functions and methods

---
+++ PY100/intro-python-book/05-functions-and-methods/11_ex.py
@@ -0,0 +1,13 @@
+# Without running the following code, what do you think it will do?
+
+def foo(first, second=3, third=2):
+    print(first)
+    print(second)
+    print(third)
+
+foo(42)
+
+# It will print the argument and the default values of 'second' (3) and of 'third' (2) parameters
+# 42
+# 3
+# 2
