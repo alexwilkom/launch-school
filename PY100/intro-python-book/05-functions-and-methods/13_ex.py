7c7ba68da33162645895e11863ccaecd8c85c710
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-23 08:32:17 +0000

    Do exercise 14, functions and methods

--- PY100/intro-python-book/05-functions-and-methods/13_ex.py
+++ PY100/intro-python-book/05-functions-and-methods/13_ex.py
@@ -1,10 +1,10 @@
 # Without running the following code, what do you think it will do?
 
-def foo(first, second=3, third):
-    print(first)
-    print(second)
-    print(third)
+# def foo(first, second=3, third):
+#     print(first)
+#     print(second)
+#     print(third)
 
-foo(42)
+# foo(42)
 
 # There is an error on the parameters definitions. When we create a default value, all the subsequent parameters must also have a default value.
