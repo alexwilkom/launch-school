823bcf735a7c25c1d95895d38cb5392d7fff6fa8
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-26 14:31:37 +0000

    Do exercise 1, intro to collections

---
+++ PY100/intro-python-book/07-intro-to-collections/01_ex.py
@@ -0,0 +1,6 @@
+# If you have a list named people, how can you determine the number of entries in that list?
+
+# Using the built-in function let(people)
+
+people = ['Alex', 'Mateo', 'Hercules']
+print(len(people))
