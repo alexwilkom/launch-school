7968059bbadc1b5adcbd42fad023c6a2844f0631
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-26 14:38:02 +0000

    Do exercise 3, intro to collections

---
+++ PY100/intro-python-book/07-intro-to-collections/03_ex.py
@@ -0,0 +1,4 @@
+# Identify at least 2 differences between lists and tuples. Identify at least 2 ways that lists and tuples are similar.
+
+# Differences: Lists are mutable, whereas tuples are not. Lists are written with [], whereas tuples with ().
+# Similarities: Lists and tuples are heterogeneous, meaning they can have different types of objects. They are ordered collections.
