5094e20283741d2919fd9946b1cc2ff5c0ed790a
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-28 15:58:04 +0000

    Do exercise 5, using collections

---
+++ PY100/intro-python-book/08-using-collections/05_ex.py
@@ -0,0 +1,11 @@
+# Which of the following values can't be used as a key in a dict object, and why?
+
+'cat'
+(3, 1, 4, 1, 5, 9, 2)
+['a', 'b']                      # <--- Mutable
+{'a': 1, 'b': 2}                # <--- Mutable
+range(5)
+{1, 4, 9, 16, 25}               # <--- Mutable
+3
+0.0
+frozenset({1, 4, 9, 16, 25})
