6992c06f2a7fb80d2fa18f630b0cf4452618a52c
Alex Wilkom <alex.wilkom@icloud.com>
2024-12-30 08:13:42 +0000

    Do exercise 1, variables as pointers

---
+++ PY100/intro-python-book/10-variables-as-pointers/01_ex.py
@@ -0,0 +1,4 @@
+# In your own words, explain the difference between these two expressions.
+
+# my_object1 == my_object2      # This check whether they have the same value
+# my_object1 is my_object2      # This check whether they are the same object in the same memory location.
