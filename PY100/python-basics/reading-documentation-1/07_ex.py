8311c3353f0dbeb62195c2eb47465e0b74c80d88
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-01 10:44:16 +0000

    Do exercise 7, reading doc 1

---
+++ PY100/python-basics/reading-documentation-1/07_ex.py
@@ -0,0 +1,6 @@
+# What happens if we take the list ['fish', 'and', 'chips'] and try to access 
+# the element at index position 10? First try to determine what will happen by 
+# consulting the documentation, then verify your understanding in the Python 
+# REPL.
+
+['fish', 'and', 'chips'][10]  # Raises IndexError: list index out of range
