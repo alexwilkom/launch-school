7c81adbfcb9882bcf5fce44a37df66fea840b3c3
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-01 13:59:19 +0000

    Do exercise 4, reading doc 2

---
+++ PY100/python-basics/reading-documentation-2/04_ex.py
@@ -0,0 +1,7 @@
+# Using the datetime module in Python, how would you obtain the current date 
+# and time?
+
+from datetime import datetime
+
+current_datetime = datetime.datetime.now()
+print(current_datetime)
