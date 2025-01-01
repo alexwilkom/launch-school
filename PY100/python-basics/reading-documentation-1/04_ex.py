c4447297e93f9c2074d920625a132f1583bdbd3c
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-01 10:31:25 +0000

    Do exercise 4, reading doc 1

---
+++ PY100/python-basics/reading-documentation-1/04_ex.py
@@ -0,0 +1,10 @@
+# Is there a method to reverse a string, for example turning 'hello' into 
+# 'olleh'?
+
+greet = 'hello'
+
+# I cannot find a method. I can use slicing.
+greet_reversed = greet[::-1]
+
+print(greet)
+print(greet_reversed)
