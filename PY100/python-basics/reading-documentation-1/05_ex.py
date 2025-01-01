52173329d03e3145047f373af22bc9339e9f745b
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-01 10:33:40 +0000

    Do exercise 5, reading doc 1

---
+++ PY100/python-basics/reading-documentation-1/05_ex.py
@@ -0,0 +1,8 @@
+# Locate the documentation for the list built-in object in Python 
+# Documentation.
+
+# How can we access the second element ('and') in the list ['fish', 'and', 
+# 'chips']?
+
+my_list = ['fish', 'and', 'chips']
+print(my_list[1])
