7617efb6a2babe692d61a74be641bcf70b08c52f
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-01 13:30:15 +0000

    Do exercise 9, read doc 1

---
+++ PY100/python-basics/reading-documentation-1/09_ex.py
@@ -0,0 +1,8 @@
+# Referring to the official Python documentation, how would you identify the 
+# data type of the following values?
+
+print(type(23.5))                # 'float'
+print(type('Call me Ishmael.'))  # 'str'
+print(type(False))               # 'bool'
+print(type(0))                   # 'int'
+print(type(None))                # 'NoneType'
