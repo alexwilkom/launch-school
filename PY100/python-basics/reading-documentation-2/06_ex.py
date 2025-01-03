a0f9e47e33f7638223a8563d84d474763c7c9c70
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-03 06:47:27 +0000

    Do exercise 6, reading doc

---
+++ PY100/python-basics/reading-documentation-2/06_ex.py
@@ -0,0 +1,7 @@
+# How many arguments does the str.join method expect? What happens if you call 
+# it with fewer or more arguments?
+
+# It expects one argument. If not provided, or more is provided, it will raise 
+# a TypeError.
+
+print(" ".join()) # TypeError
