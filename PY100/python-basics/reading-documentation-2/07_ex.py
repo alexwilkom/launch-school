432ac9eec7a1d18fc74f9b98b7966d07822d1c22
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-03 07:12:16 +0000

    Do exercise 7, reading doc 2

---
+++ PY100/python-basics/reading-documentation-2/07_ex.py
@@ -0,0 +1,6 @@
+# Using the official Python documentation, can you determine how to check 
+# whether a string contains a specific substring?
+
+# We can use the operator 'in'
+print('le' in 'Alex')  # True
+print('ls' in 'Alex')  # False
