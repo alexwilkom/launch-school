9564154c68cb8bf6ac93340c074f991de5d8b024
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-03 08:16:07 +0000

    Do exercise 8, loops

---
+++ PY100/python-basics/loops/08_ex.py
@@ -0,0 +1,12 @@
+# The following code keeps looping forever. (You can hit Ctrl-C to stop it.) 
+# Why does the loop keep running? Modify it so that it stops after the first 
+# iteration.
+
+# while True:
+#     print("and on")
+
+# The condition is always true. Therefore, it never ends.
+
+while True:
+    print("and on")
+    break
