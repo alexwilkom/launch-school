3e0c25f8f85a19475bc12107084f1f75f78e316e
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-04 17:25:53 +0000

    Do exercise 1, conditionals

---
+++ PY100/python-basics/conditionals/01_ex.py
@@ -0,0 +1,15 @@
+# Without looking at your notes or the official documentation, try to recall 
+# all values that count as falsy in Python.
+
+0
+''
+[]
+()
+{}
+None
+False
+
+# from solution
+0.0, 0j
+set()
+range(0)
