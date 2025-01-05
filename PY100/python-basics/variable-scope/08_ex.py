86549c08d2c19874becf2dbd04ca46180c664169
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-05 10:24:01 +0000

    Do exercise 8, variable scope

---
+++ PY100/python-basics/variable-scope/08_ex.py
@@ -0,0 +1,8 @@
+# What will the following code do and why? Don't run it until you have tried 
+# to answer.
+
+# print(greeting) # NameError
+
+# We define the variable greeting after it tries to access it.
+
+greeting = 'Hello world!'
