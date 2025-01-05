e10367022f774d85155222adfc30e08ac43a736f
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-05 10:08:36 +0000

    Do exercise 3, variable scope

---
+++ PY100/python-basics/variable-scope/03_ex.py
@@ -0,0 +1,13 @@
+# What will the following code do and why? Don't run it until you have tried 
+# to answer.
+
+def my_function():
+    a = 1
+
+    if True:
+        print(a)
+
+my_function() # 1
+
+# The function can print 1 because we have initialised a local variable. It is 
+# in the scope of the function.
