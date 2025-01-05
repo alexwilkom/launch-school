6f66f2a8119e3dd63bf49037af46c84f77b7420c
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-05 10:13:36 +0000

    Do exercise 4, variable scope

---
+++ PY100/python-basics/variable-scope/04_ex.py
@@ -0,0 +1,13 @@
+# What will the following code do and why? Don't run it until you have tried 
+# to answer.
+
+a = 1
+
+def my_function():
+    print(a)
+
+my_function() # 1
+
+# We can access 'a' but we cannot reassign it or make changes without the 
+# global keyword.
+
