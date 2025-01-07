013389ab5b3458fe59d5ab2356465a1a0b9f0ed4
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-07 07:37:59 +0000

    Do exercise 8, strings

---
+++ PY100/python-basics/strings/08_ex.py
@@ -0,0 +1,6 @@
+# Write code that capitalizes the words in the string 'launch school tech & 
+# talk', so that you get the string 'Launch School Tech & Talk'.
+
+my_string = 'launch school tech & talk'
+titled_string = my_string.title()
+print(titled_string)
