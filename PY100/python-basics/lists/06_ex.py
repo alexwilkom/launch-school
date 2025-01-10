4eae3fbc33f1bf22e90fc762be003cd441353aa6
Alex Wilkom <alex.wilkom@icloud.com>
2025-01-10 07:18:37 +0000

    Do exercise 6, lists

---
+++ PY100/python-basics/lists/06_ex.py
@@ -0,0 +1,21 @@
+# You've been given a list of vocabulary words grouped into sub-lists, by 
+# meaning. This is a two-dimensional list or a nested list. Write some code 
+# that iterates through and prints all the words, one per line.
+
+vocabulary = [
+    ['happy', 'cheerful', 'merry', 'glad'],
+    ['tired', 'sleepy', 'fatigued', 'drained'],
+    ['excited', 'eager', 'enthused', 'animated'],
+]
+
+# happy
+# cheerful
+# merry
+# glad
+# tired
+# sleepy
+# etc...
+
+for sub_list in vocabulary:
+    for word in sub_list:
+        print(word)
