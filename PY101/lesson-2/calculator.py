f42e038c9def1e26cc11a0c7788797a2f5c7a809
Alex Wilkom <alex.wilkom@icloud.com>
2025-02-10 21:27:10 +0000

    Add validation for number input

--- PY101/lesson-2/calculator.py
+++ PY101/lesson-2/calculator.py
@@ -7,15 +7,31 @@
 def prompt(message):
     print(f"=> {message}")
 
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
 prompt('Welcome to Calculator!')
 
 # Ask the user for the first number
 prompt("What's the first number?")
 number1 = input()
 
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
 # Ask the user for the second number
 prompt("What's the second number?")
 number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
 
 prompt('What operation would you like to perform?\n'
       '1) Add 2) Subtract 3) Multiply 4) Divide')
