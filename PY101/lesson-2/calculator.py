af24af13a0fb74da291a452f01055f0c87623bf4
Alex Wilkom <alex.wilkom@icloud.com>
2025-02-15 12:10:39 +0000

    Add ask-another-calculation feature

--- PY101/lesson-2/calculator.py
+++ PY101/lesson-2/calculator.py
@@ -9,38 +9,55 @@ def invalid_number(number_str):
 
     return False
 
-prompt('Welcome to Calculator!')
-
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+def ask_numbers():
+    prompt("What's the first number?")
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt("What's the second number?")
     number2 = input()
 
-prompt('What operation would you like to perform?\n'
-      '1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3, or 4')
+    return (number1, number2)
+
+def ask_operation():
+    prompt('What operation would you like to perform?\n'
+        '1) Add 2) Subtract 3) Multiply 4) Divide')
     operation = input()
 
-match operation:
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output = int(number1) * int(number2)
-    case '4':
-        output = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt('You must choose 1, 2, 3, or 4')
+        operation = input()
 
-prompt(f"The result is: {output}")
+    return operation
+
+def perform_operation(numbers, operation):
+    match operation:
+        case '1':
+            output = int(numbers[0]) + int(numbers[1])
+        case '2':
+            output = int(numbers[0]) - int(numbers[1])
+        case '3':
+            output = int(numbers[0]) * int(numbers[1])
+        case '4':
+            output = int(numbers[0]) / int(numbers[1])
+
+    return output
+
+prompt('Welcome to Calculator!')
+
+while True:
+    result = perform_operation(ask_numbers(), ask_operation())
+    prompt(f'The result is: {result}')
+
+    prompt('Would you like to perform another operation?\n'
+           'Type y if yes. Reply anything else if not.')
+    answer = input()
+    if answer.lower() != 'y':
+        break
