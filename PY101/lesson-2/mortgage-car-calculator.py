db36c1361f2ac1c8dc897a3b9bb04767043cfbf5
Alex Wilkom <alex.wilkom@icloud.com>
2025-02-22 12:17:52 +0000

    Add a loop to perform another calculation

--- PY101/lesson-2/mortgage-car-calculator.py
+++ PY101/lesson-2/mortgage-car-calculator.py
@@ -1,5 +1,5 @@
 def prompt(message):
-    print(f'=> {message}')
+    print(f'\n=> {message}')
 
 def invalid_number(number_str):
     try:
@@ -20,15 +20,26 @@ def ask_number(message):
     
     return float(number)
 
-loan_amount = ask_number('What is the loan amount?')
-interest_rate = ask_number('What is the interest rate? Example: 5.5 for 5.5%')
-loan_duration_years = ask_number('What is the loan duration in years?')
+while True:
+    loan_amount = ask_number('What is the loan amount?')
+    interest_rate = ask_number('What is the interest rate? Example: 5.5 for 5.5%')
+    loan_duration_years = ask_number('What is the loan duration in years?')
 
-monthly_interest_rate = interest_rate / 100 / 12
-loan_duration_months = loan_duration_years * 12
-monthly_payment = loan_amount * (
-        monthly_interest_rate /
-            (1 - (1 + monthly_interest_rate) ** (-loan_duration_months))
-    )
+    monthly_interest_rate = interest_rate / 100 / 12
+    loan_duration_months = loan_duration_years * 12
+    monthly_payment = loan_amount * (
+            monthly_interest_rate /
+                (1 - (1 + monthly_interest_rate) ** (-loan_duration_months))
+        )
 
-prompt(f"The monthly payment is: ${round(monthly_payment, 2)}")
+    prompt(f'The monthly payment is: ${round(monthly_payment, 2)}')
+
+    while True:
+        answer = input('\nDo you want to perform another calculation? (y/n) ').strip().lower()
+        if answer.startswith(('y', 'n')):
+            break
+        prompt('Please enter "y" or "n".')
+
+    if answer.startswith('n'):
+        print('\nThank you for using the Mortgage/Car Calculator!')
+        break
