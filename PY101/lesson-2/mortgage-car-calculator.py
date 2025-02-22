b7bc2f6feaaca8fb8dd0d98fc3b7fc31a04ff203
Alex Wilkom <alex.wilkom@icloud.com>
2025-02-22 11:40:42 +0000

    Check for negative numbers

--- PY101/lesson-2/mortgage-car-calculator.py
+++ PY101/lesson-2/mortgage-car-calculator.py
@@ -3,7 +3,9 @@ def prompt(message):
 
 def invalid_number(number_str):
     try:
-        float(number_str)
+        number = float(number_str)
+        if number <= 0:
+            raise ValueError
     except ValueError:
         return True
 
@@ -12,19 +14,19 @@ def invalid_number(number_str):
 prompt('What is the loan amount?')
 loan_amount = input()
 while invalid_number(loan_amount):
-    prompt('Please enter a valid number (do not use a comma).')
+    prompt('Please enter a positive number.')
     loan_amount = input()
 
 prompt('What is the interest rate?')
 interest_rate = input()
 while invalid_number(interest_rate):
-    prompt('Please enter a valid number (positive whole or decimal numbers).')
+    prompt('Please enter a positive number.')
     interest_rate = input()
 
 prompt('What is the loan duration in years?')
 loan_duration_years = input()
 while invalid_number(loan_duration_years):
-    prompt('Please enter a valid number (whole positive numbers).')
+    prompt('Please enter a positive number.')
     loan_duration_years = input()
 
 monthly_interest_rate = float(interest_rate) / 100 / 12
