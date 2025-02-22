6ce40093f4538c16acc8cda3adf9a77aa8e7456b
Alex Wilkom <alex.wilkom@icloud.com>
2025-02-22 11:30:05 +0000

    Calculate the monthly payment

--- PY101/lesson-2/mortgage-car-calculator.py
+++ PY101/lesson-2/mortgage-car-calculator.py
@@ -15,14 +15,21 @@ while invalid_number(loan_amount):
     prompt('Please enter a valid number (do not use a comma).')
     loan_amount = input()
 
-prompt('What is the Annual Percentage Rate (APR)?')
-apr = input()
-while invalid_number(apr):
+prompt('What is the interest rate?')
+interest_rate = input()
+while invalid_number(interest_rate):
     prompt('Please enter a valid number (positive whole or decimal numbers).')
-    apr = input()
+    interest_rate = input()
 
 prompt('What is the loan duration in years?')
 loan_duration_years = input()
 while invalid_number(loan_duration_years):
     prompt('Please enter a valid number (whole positive numbers).')
     loan_duration_years = input()
+
+monthly_interest_rate = float(interest_rate) / 100 / 12
+loan_duration_months = float(loan_duration_years) * 12
+monthly_payment = float(loan_amount) * (
+        monthly_interest_rate /
+            (1 - (1 + monthly_interest_rate) ** (-loan_duration_months))
+    )
