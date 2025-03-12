b126452ac7d2fb35a2acd0c18ffff6e73ffba86c
Alex Wilkom <alex.wilkom@icloud.com>
2025-03-12 11:49:03 +0000

    Refactor to confirm with pylint warnings

--- PY101/lesson-2/rock_paper_scissors.py
+++ PY101/lesson-2/rock_paper_scissors.py
@@ -2,39 +2,33 @@ import random
 
 VALID_CHOICES = ['rock', 'paper', 'scissors', "lizard", "spock"]
 
+WINNING_COMBOS = {
+    'rock':     ['scissors', 'lizard'],
+    'paper':    ['rock',     'spock'],
+    'scissors': ['paper',    'lizard'],
+    'lizard':   ['paper',    'spock'],
+    'spock':    ['rock',     'scissors'],
+}
+
+def player_wins(player, computer):
+    return computer in WINNING_COMBOS[player]
+
 def prompt(message):
     print(f"==> {message}")
-
-def determine_winner(player, computer):
-    if ((player == "rock" and
-         (computer == "scissors" or computer == "lizard")) or
-        (player == "paper" and
-         (computer == "rock" or computer == "spock")) or
-        (player == "scissors" and
-         (computer == "paper" or computer == "lizard")) or
-        (player == "lizard" and
-         (computer == "paper" or computer == "spock")) or
-        (player == "spock" and
-         (computer == "rock" or computer == "scissors"))):
-        return player
-    elif player == computer:
-        return None
-    else:
-        return computer
 
 def display_winner(player, computer):
     prompt(f"You chose {player}, computer chose {computer}")
 
-    if determine_winner(player, computer) == player:
-        prompt("You win!")
-    elif determine_winner(player, computer) == computer:
-        prompt("Computer wins!")
-    else:
+    if player_wins(player, computer):
+        prompt('You win!')
+    elif player == computer:
         prompt("It's a tie!")
+    else:
+        prompt("Computer wins!")
 
-def display_scores(player_score, computer_score):
-    prompt(f'Your score: {player_score}')
-    prompt(f'Computer score: {computer_score}')
+def display_scores(player, computer):
+    prompt(f'Your score: {player}')
+    prompt(f'Computer score: {computer}')
 
 player_score = 0
 computer_score = 0
@@ -50,14 +44,16 @@ while True:
     computer_choice = random.choice(VALID_CHOICES)
 
     display_winner(choice, computer_choice)
-    
-    if determine_winner(choice, computer_choice) == choice:
+
+    if player_wins(choice, computer_choice):
         player_score += 1
-    elif determine_winner(choice, computer_choice) == computer_choice:
+    elif choice == computer_choice:
+        pass
+    else:
         computer_score += 1
 
     display_scores(player_score, computer_score)
-    
+
     if player_score == 5:
         prompt('You have won the game!')
     elif computer_score == 5:
@@ -71,10 +67,10 @@ while True:
             player_score = 0
             computer_score = 0
             break
-        elif answer.startswith('n'):
+        if answer.startswith('n'):
             break
-        else:
-            prompt("That's not a valid choice")
-        
+
+        prompt("That's not a valid choice")
+
     if player_score == 5 or computer_score == 5:
         break
